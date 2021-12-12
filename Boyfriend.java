939c69b684b7f3b9bf45acf6138e2e378d44333e
NukerFall <47152422+NukerFall@users.noreply.github.com>
2021-12-12 23:09:12 +0300

    Create Boyfriend.java

---
+++ Boyfriend.java
@@ -0,0 +1,13 @@
+package me.carefall.coding;
+
+public class Boyfriend extends Coder {
+
+    public Boyfriend() {
+        setLevel("senior");
+        setProgramming(Knowledge.getSeniorJavaKnowledge());
+    }
+    
+    /*
+    * code...
+    */
+}
