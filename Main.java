2abc6f873c0330b60531f301830c18594e924e39
NukerFall <47152422+NukerFall@users.noreply.github.com>
2021-12-12 23:01:59 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package me.carefall.coding;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // beggining of universe
+    	Boyfriend bf = new Boyfriend();
+    	Girlfriend gf = new Girlfriend();
+        bf.teachProgramming(gf);
+        // other things to do
+    }
+
+    /*
+    *  code...
+    */
+}
