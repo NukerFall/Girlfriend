7d4b5887d9e4f1eb424e0b274f667884f322e89a
NukerFall <47152422+NukerFall@users.noreply.github.com>
2021-12-12 23:03:30 +0300

    Create Knowledge.java

---
+++ Knowledge.java
@@ -0,0 +1,25 @@
+package me.carefall.coding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Knowledge {
+
+    public static List<String> getSeniorJavaKnowledge() {
+        List<String> knowledge = new ArrayList<String>();
+        knowledge.add("IDE");
+        knowledge.add("VARIABLES");
+        knowledge.add("TYPES");
+        knowledge.add("OBJECTS");
+        knowledge.add("LOGIC");
+        knowledge.add("MATHS");
+        knowledge.add("INHERITANCE");
+        knowledge.add("CYCLES");
+        knowledge.add("ARRAYS");
+        knowledge.add("LISTS");
+        knowledge.add("MAPS");
+        knowledge.add("THREADS");
+        // who knows how much stuff do coders know...
+        return knowledge;
+    }
+}
